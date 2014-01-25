0f6cf2c29d537b6ccf094e9a1d28aedd9be0dbc0
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 19:07:15 +0100

    Added the finishing texts.

---
+++ Assets/scripts/EndLevel.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+    GameObject player; 
+    GameObject finish; 
+
+    bool playerKilled_; 
+    bool playerFinished_; 
+
+    public void setPlayerKilled(bool p) 
+    { 
+        playerKilled_ = p; 
+    } 
+
+    public void setPlayerFinished(bool p) 
+    { 
+        playerFinished_ = p; 
+    } 
+
+	// Use this for initialization
+	void Start () { 
+        player = GameObject.Find("Player");
+        finish = GameObject.Find("Finish");
+
+        playerKilled_ = false;
+        playerFinished_ = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (player.GetComponent<Character>().getPlayerHp() <= 0)
+            //DisplayResult(); 
+	} 
+
+    void DisplayResult() 
+    {
+        if (playerKilled_ == true)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - 100, Screen.height / 2, 400, 50)); 
+            GUILayout.Label("You died... you miserable piece of meat...");
+            GUILayout.EndArea();
+        } 
+        else if (playerFinished_ == true)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - 100, Screen.height / 2, 400, 50)); 
+            GUILayout.Label("You have survived... for now..."); 
+            GUILayout.EndArea(); 
+        }
+    } 
+
+    void OnGUI() 
+    { 
+        DisplayResult(); 
+    } 
+}
