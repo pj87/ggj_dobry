8e4f51638805227ec8ad5adb58d9503e798df2eb
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 01:45:50 +0100

    Simple wall.

--- Assets/CreateMap.cs
+++ Assets/CreateMap.cs
@@ -11,14 +11,17 @@ public class CreateMap : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        for (int y = 0; y < 5; y++)
-        {
-            for (int x = 0; x < 5; x++)
+        //for (int y = 0; y < 5; y++)
+        //{
+            for (int x = 0; x < 500; x++)
             {
-                var tmp = Instantiate(brick, new Vector3(x, 0, y), Quaternion.identity) as Transform;
-                lista.Add(tmp);
+                var tmp = Instantiate(brick, new Vector3(x, 1.0f, -10.0f), Quaternion.identity) as Transform;
+                var tmp1 = Instantiate(brick, new Vector3(-10.0f, 1.0f, x), Quaternion.identity) as Transform;
+                var tmp2 = Instantiate(brick, new Vector3(x, 1.0f, 10.0f), Quaternion.identity) as Transform;
+                var tmp3 = Instantiate(brick, new Vector3(10.0f, 1.0f, x), Quaternion.identity) as Transform;
+                //lista.Add(tmp);
             }
-        } 
+        //} 
 	}
 	
 	// Update is called once per frame
