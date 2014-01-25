12e849f69d2cfed80314d7e81cc6acfcd2b171ab
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 12:29:06 +0100

    Reloading the weapon works.

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -10,17 +10,24 @@ public class Shooting : MonoBehaviour
 
     public List<GameObject> lista = new List<GameObject>();
     public AudioClip shotSound;
+    public AudioClip reloadSound;
+
+    int numBullets;
+    bool shooting; 
+
     // Use this for initialization 
     void Start()
     {
+        numBullets = 5;
+        shooting = true; 
         //player = GameObject.Find("3rd Person Controller");
         //player.transform.position. 
-    }
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl) && shooting == true) 
         {
             var b = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
 
@@ -29,10 +36,25 @@ public class Shooting : MonoBehaviour
 
             Debug.Log("transform.forward: " + transform.forward);
 
+            numBullets--; 
+
             audio.PlayOneShot(shotSound);
             //audio.Play(shotSound); 
 
             lista.Add(b);
         }
+        
+        if (numBullets <= 0)
+        {
+            shooting = false;
+            audio.PlayOneShot(reloadSound);
+            numBullets = 5; 
+            Invoke("reloadFinished", 1); 
+        } 
+    } 
+
+    void reloadFinished()
+    {
+        shooting = true;
     }
 } 
