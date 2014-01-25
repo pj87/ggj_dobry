09b1ebac894360b725f54ebc92d1f7389cc867b3
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 10:58:54 +0100

    Started working on shooting.

---
+++ Assets/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+public class Shooting : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Vector3 speed;
+    //public GameObject weapon;
+
+    public List<GameObject> lista = new List<GameObject>();
+
+    // Use this for initialization 
+    void Start()
+    {
+        //player = GameObject.Find("3rd Person Controller");
+        //player.transform.position. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            var b = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+
+            b.transform.Rotate(new Vector3(90, transform.rotation.eulerAngles.y, 180));
+            b.rigidbody.AddForce(transform.forward * 80);
+
+
+            lista.Add(b);
+        }
+    }
+} 
