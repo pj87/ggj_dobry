114b4131b33748455c805b49bfa543d83ba48945
Qawery <wooki1@op.pl>
2014-01-25 20:19:17 +0100

    Dodano obrazenia i HP do przeciwnikow

--- Assets/GlutBehavior.cs
+++ Assets/GlutBehavior.cs
@@ -4,4 +4,13 @@ using System.Collections;
 public class GlutBehavior : EnemyBehaviour 
 {
 	public override int damage{ get {return 30;}}
+	public override int maxEnemyHP{ get {return 3;}}
+
+	public override void alertSound()
+	{
+	}
+
+	public override void death()
+	{
+	}
 }
--- Assets/HumanoidBehavior.cs
+++ Assets/HumanoidBehavior.cs
@@ -4,4 +4,13 @@ using System.Collections;
 public class HumanoidBehavior : EnemyBehaviour 
 {
 	public override int damage{ get {return 20;}}
+	public override int maxEnemyHP{ get {return 1;}}
+	
+	public override void alertSound()
+	{
+	}
+	
+	public override void death()
+	{
+	}
 }
