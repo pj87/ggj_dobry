11a17f9fa1e6fa867e12a9ffb7a2d739258a77b8
Qawery <wooki1@op.pl>
2014-01-25 03:35:20 +0100

    Nowa scena do testów wrogów

---
+++ Assets/enemyChaseScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyChaseScript : MonoBehaviour 
+{
+	private NavMeshAgent agent;
+	private GameObject player;
+	private int i = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag("Player");
+		//InvokeRepeating ("pathUpdate", 0, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( (i++)%30 == 0) 
+		{
+			agent.SetDestination (player.transform.position);
+		}
+	}
+
+	void pathUpdate()
+	{
+		//agent.SetDestination (player.transform.position);
+	}
+}
