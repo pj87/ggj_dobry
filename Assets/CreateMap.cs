ba9855dd95031c95d587aa0924b1c11b046398ff
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 10:17:45 +0100

    Generatring the comllumns.

--- Assets/CreateMap.cs
+++ Assets/CreateMap.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 
 public class CreateMap : MonoBehaviour {
 
-    public Transform brick; 
+    //public Transform brick; 
     public Transform collumn; 
     //public Transform player;
     //To jest caly murek w 1 miejscu. CYA!
     List<Transform> lista = new List<Transform>(); 
 
 	// Use this for initialization
-	void create_borders()
+	/*
+    void create_borders()
     {
         for (int x = -50; x <= 50; x++)
         {
@@ -21,20 +22,20 @@ public class CreateMap : MonoBehaviour {
             var tmp3 = Instantiate(brick, new Vector3(50.0f, 1.0f, x), Quaternion.identity) as Transform;
             //lista.Add(tmp);
         }
-    }
+    }*/ 
     
     void create_columns()
-    {
-        for (int i = 0; i < 500; i++)
-        {
-            int x = Random.Range(-50, 49);
-            int y = Random.Range(-50, 49); 
+    { 
+        for (int i = 0; i < 100; i++)
+        { 
+            int x = Random.Range(-49, 49);
+            int y = Random.Range(-49, 49); 
             var tmp = Instantiate(collumn, new Vector3(x, 1.0f, y), Quaternion.identity) as Transform;
         } 
-    }
+    } 
 
     void Start () {
-        create_borders();
+        //create_borders();
         create_columns(); 
 	}
 	
