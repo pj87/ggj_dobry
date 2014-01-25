e9bbaa96b232b4a2794354b1e1def5dfd589961f
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-25 11:42:27 +0100

    The bullets are destroyed when colliding.

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //collision.collider.gameObject 
+        Debug.Log("Collision with other objects...");
+        Destroy(gameObject);
+    } 
+}
