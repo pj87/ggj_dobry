42f3837dc3784d05b10ad31b0bec4b7e82b7348e
Arek Dygas <szafirr@gmail.com>
2014-01-26 10:52:12 +0100

    HP fixed, blob - new model version imported

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -26,7 +26,8 @@ public class Character : MonoBehaviour {
 	public AudioClip playerStepB;
 	public AudioClip playerStepFull;
 
-	private int playerHP = 100;
+    public int maxPlayerHP = 100;
+	private int playerHP;
 	private float speed = 1.5f;
     private GameObject mainCamera_; 
 
@@ -38,6 +39,8 @@ public class Character : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
+        playerHP = maxPlayerHP;
+
 		//ustawienie wizje na latarke
 		visionALongLight.enabled = true;
 		visionAShortLight.enabled = true;
--- Assets/scripts/GUIScript.cs
+++ Assets/scripts/GUIScript.cs
@@ -5,17 +5,30 @@ public class GUIScript : MonoBehaviour {
     public GUIStyle style; 
     float life; 
     GameObject player;
+    Character character;
+    private bool _isStyleSet;
+    private GUIStyle _guiStyle;
 
     void Start()
     {
-        player = GameObject.Find("Player"); 
+        player = GameObject.Find("Player");
+        character = player.GetComponent<Character>();
     }
 
     void OnGUI() 
     {
-        life = player.GetComponent<Character>().getPlayerHp(); 
+        if (!_isStyleSet)
+        {
+            _guiStyle = new GUIStyle(GUI.skin.label);
+            _guiStyle.fontSize = 20;
+            _guiStyle.fontStyle = FontStyle.Bold;
+            _guiStyle.normal.textColor = new Color(0.8f, 0.1f, 0.1f);
+            _guiStyle.alignment = TextAnchor.UpperRight;
+        }
+        life = character.getPlayerHp();
 
-        GUI.Box(new Rect(10, 10, Screen.width / (2 / (life / 100.0f)), 25), life + "/" + 100); 
+        GUI.Label(new Rect(10, 10, 50, 40), "HP:", _guiStyle);
+        GUI.Label(new Rect(60, 10, 80, 40), string.Format("{0}/{1}", life, character.maxPlayerHP), _guiStyle);
 
         //GUILayout.BeginArea(new Rect(Screen.width-200, 0, 5000, 500)); 
         //Tutaj można sobie forem wyświetlać jakieś fajne listy 
